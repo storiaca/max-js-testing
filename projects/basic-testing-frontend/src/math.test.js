313ac84b3d5934954786799ac4d45d4364e32b48
Aleksandar Ristic <storiaca@gmail.com>
2024-01-19 09:34:08 +0100

    Testing for throw errors and error message

--- projects/basic-testing-frontend/src/math.test.js
+++ projects/basic-testing-frontend/src/math.test.js
@@ -43,10 +43,16 @@ it("should throw an error if no value is passed into the function", () => {
     add();
   };
 
-  expect(resultFn).toThrow();
+  expect(resultFn).toThrow(/is not iterable/);
 });
 
 it("should throw an error if provided with multiple arguments instead of an array", () => {
   const num1 = 1;
   const num2 = 2;
+
+  const resultFn = () => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow(/is not iterable/);
 });
